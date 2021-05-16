244bf8038f9d4e23836d9f3849173b4b6061f3b0
satyampgt4 <satyamgpt44@gmail.com>
2021-05-16 11:46:20 +0530

    add perpendiculer distance

--- appjs/logicjavascript.js
+++ appjs/logicjavascript.js
@@ -2688,32 +2688,32 @@ function pppSolve() {
 }
 
 
-function pppdSolve() {
-    var a=parseFloat(document.getElementById('pppdaq2').value);
-    var b=parseFloat(document.getElementById('pppdbq2').value);
-    var c=parseFloat(document.getElementById('pppdcq1').value);
-    var d=parseFloat(document.getElementById('pppdcq2').value);
+function pppdoSolve() {
+    var a=parseFloat(document.getElementById('pppdaqo1').value);
+    var b=parseFloat(document.getElementById('pppdbqo1').value);
+    var c=parseFloat(document.getElementById('pppdcqo1').value);
     
    
     var dis = ((a*a)+(b*b));
-    var p =Math.abs(c-d);
+    var p =Math.abs(c);
 
     if( isNaN(d) || isNaN(a) || isNaN(b) || isNaN(c)){
-        document.getElementById('pppdcq1output').innerHTML= "\\[ Please \\space enter \\space all \\space input \\]";
-        renderMathInElement(document.getElementById("pppdcq1output"));
-        document.getElementById('pppdcq1explane').innerHTML = "";
-        renderMathInElement(document.getElementById("pppdcq1explane"));
+        document.getElementById('pppdcq1outputo').innerHTML= "\\[ Please \\space enter \\space all \\space input \\]";
+        renderMathInElement(document.getElementById("pppdcq1outputo"));
+        document.getElementById('pppdcq1explaneo').innerHTML = "";
+        renderMathInElement(document.getElementById("pppdcq1explaneo"));
     } else{
-        document.getElementById('pppdcq1explane').innerHTML = "\\[ Distance \\space = \\space \\frac{ | \\space "+ c+ "\\space - \\space "+ d+ " | }{ \\sqrt { ("+ a +" )^2 \\space + \\space ("+ b +")^2 \\space } } \\space  \\]";
-        renderMathInElement(document.getElementById("pppdcq1explane"));
+        document.getElementById('pppdcq1explaneo').innerHTML = "\\[ Distance \\space = \\space \\frac{ | \\space "+ c+ " | }{ \\sqrt { ("+ a +" )^2 \\space + \\space ("+ b +")^2 \\space } } \\space  \\]";
+        renderMathInElement(document.getElementById("pppdcq1explaneo"));
 
         if(!(Number.isInteger(Math.sqrt(dis))))
-        document.getElementById('pppdcq1output').innerHTML = "\\[ Distance \\space = \\space \\frac{ "+  p+ " }{ \\sqrt { "+ dis + "  } } \\space  \\]";
+        document.getElementById('pppdcq1outputo').innerHTML = "\\[ Distance \\space = \\space \\frac{ "+  p+ " }{ \\sqrt { "+ dis + "  } } \\space  \\]";
         else
-        document.getElementById('pppdcq1output').innerHTML = "\\[ Distance \\space = \\space \\frac{ "+ p+ " }{ "+ Math.sqrt(dis) +" } \\space  \\]";
-        renderMathInElement(document.getElementById("pppdcq1output"));
+        document.getElementById('pppdcq1outputo').innerHTML = "\\[ Distance \\space = \\space \\frac{ "+ p+ " }{ "+ Math.sqrt(dis) +" } \\space  \\]";
+        renderMathInElement(document.getElementById("pppdcq1outputo"));
     }
 }
+
 
 function splittrifind(){
     let side = parseInt(document.getElementById('splittri').value);
